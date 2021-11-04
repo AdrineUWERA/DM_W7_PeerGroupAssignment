a452d368ccbd573212753ac1a4b3edd5efe28a32
Adrine UWERA <a.uwera@alustudent.com>
2021-11-04 19:24:47 +0200

    improved the outuputs

--- Question1.py
+++ Question1.py
@@ -2,7 +2,7 @@
 def set_repr():
     list1 = list((input("\nEnter a list of elements separated by a blank space : ").strip().split()))
     if len(list1) == len(set(list1)):
-        print("True")
+        print("True. The entered list constitutes a set.")
 
     else:
         output_set = set(list1)
@@ -10,3 +10,4 @@ def set_repr():
 
 
 set_repr()
+
